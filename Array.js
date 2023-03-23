3540103f483de8d7a5dc2a1be22400f814445029
AlejoVR09 <Alejovanegas09@outlook.com>
2023-03-23 02:33:16 +0100

    Termina ejercicos basicos con Arrays

--- Array.js
+++ Array.js
@@ -1,3 +1,12 @@
 var array = [1, 2, 3, 4]
 var longitud = array.length
 console.log(longitud)
+
+var array_1 = [1, 2, 3, 4]
+var lastElement = array_1.pop()
+console.log(lastElement)
+console.log(array_1)
+
+function solution(arraySecreto) {
+    return (typeof arraySecreto[0] === "string" ? true : false)
+}
